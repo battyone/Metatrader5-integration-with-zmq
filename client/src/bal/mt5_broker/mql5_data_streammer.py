b59da12be9e93f15ccfee854e2b0293c87663990
Leoni Mota Loris <leoniloris1@hotmail.com>
2020-02-05 23:49:29 -0300

    Most men pursue pleasure with such breathless haste that they hurry past it.

--- client/src/bal/mt5_broker/mql5_data_streammer.py
+++ client/src/bal/mt5_broker/mql5_data_streammer.py
@@ -12,23 +12,32 @@ class MQL5DataStreammer:
         self._context = zmq.Context()
         self._server_closed = server_closed
         self._subscription_queue = Queue()
-        # self._sockets_sub = {}
+        self._port_to_symbol = {}
 
-    def _blocking_polling_data(self, socket):
+    def _blocking_polling_data(self, socket, port):
         while not self._server_closed.is_set():
             data = socket.recv().decode("utf-8").split("|")
-            self._subscription_queue.put(SubscriptionData(data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+            self._subscription_queue.put(SubscriptionData(
+                self._port_to_symbol[port], data[0], data[1], data[2], data[3], data[4], data[5]))
         socket.close()
 
+    @staticmethod
+    def _wait_for_server():
+        # This workaround exists to wait until a server is existent.
+        import time
+        time.sleep(3)
 
-    def add_subscription(self):
+    def add_subscription(self, symbol):
         self._request_port += 1
         available_port = self._request_port
-
+        self._port_to_symbol[symbol] = available_port
+        self._wait_for_server()
         socket = self._context.socket(zmq.SUB)
         socket.connect('%s:%s' % (self._server_hostname, available_port))
         socket.setsockopt(zmq.SUBSCRIBE, b'')
-        Thread(target=self._blocking_polling_data, args=(socket,), daemon=True).start()
+        Thread(target=self._blocking_polling_data,
+               args=(socket, available_port),
+               daemon=True).start()
 
     def request_data(self):
         return self._subscription_queue.get()
--- server/tick_subscriber.mq5
+++ server/tick_subscriber.mq5
@@ -16,30 +16,25 @@ const uchar max_symbol_name_size = 6;
 Context context("tick_subscriber");
 ZMQ_api zmq(context);
 MqlTick tick;
-uchar event_data[40] = {0};
+uchar event_data[60] = {0};
 
 int OnInit(void) {
     zmq.setup_pub_server(ZEROMQ_PROTOCOL, HOSTNAME, port);
-    StringToCharArray(_Symbol, event_data, 0, max_symbol_name_size); // I have 6 bytes
-    
     return(INIT_SUCCEEDED);
 }
 
-
 int OnCalculate(const int rates_total, const int prev_calculated, const int begin, const double& price[]) {
   SymbolInfoTick(_Symbol, tick);
-  Print(tick.last);
-  string event_data = StringFormat("%s|%s|%.2f|%.2f|%.2f|%d|%d", _Symbol, IntegerToString(tick.time_msc), tick.bid, tick.ask, tick.last, tick.volume, tick.flags);
-  
+  StringToCharArray(
+      StringFormat("%d|%.2f|%.2f|%.2f|%d|%d|",
+                   tick.time_msc,
+                   tick.bid,
+                   tick.ask,
+                   tick.last,
+                   tick.volume,
+                   tick.flags),
+      event_data, 0);
+
   zmq.publish(event_data);
-  // EventChartCustom(chart_id, custom_event_id, (long)CHARTEVENT_TICK, 0.0, event_data);
-  
-  /*if ((tick.flags & 8) == 8 || (tick.flags & 16) == 16 || (tick.flags & 32)==32 || (tick.flags & 64) == 64) {
-      Print(tick.last);
-      string event_data = StringFormat("%s|%s|%.3f|%.3f|%.3f|%d|%d", _Symbol, IntegerToString(tick.time_msc), tick.bid, tick.ask, tick.last, tick.volume, tick.flags);
-      EventChartCustom(chart_id, custom_event_id, (long)CHARTEVENT_TICK, 0.0, event_data);
-  }*/
-  
-  
   return 0;
 }
